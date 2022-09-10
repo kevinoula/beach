d63c5402882182ca2fd91c2e69b54f2730f64507
kevin-oula <k_oula@apple.com>
2022-09-10 16:34:40 -0400

    added ssh functionality

--- collection/collection.go
+++ collection/collection.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/kevinoula/beach/log"
+	"github.com/kevinoula/beach/shell"
 	"io/fs"
 	"io/ioutil"
 	"os"
@@ -83,14 +84,14 @@ type Credentials struct {
 
 // AddShell takes some credentials and adds them to the existing collection of SSH configurations. Credentials consist
 // of a username, password, and a hostname. The password is encoded.
-func (c *Collection) AddShell(creds Credentials) {
+func (c *Collection) AddShell(ssh shell.SSH) {
 	shellConfig := map[string]map[string]map[string]string{"shells": {}} // Init an empty shell map for JSON
 	if len(c.ShellCollection.Shells) > 0 {                               // Add collected shells
 		shellConfig["shells"] = c.ShellCollection.Shells
 	}
-	shellConfig["shells"][creds.Hostname] = map[string]string{ // Add the new credentials
-		"username": creds.Username,
-		"password": creds.Password,
+	shellConfig["shells"][ssh.Hostname] = map[string]string{ // Add the new credentials
+		"username": ssh.Username,
+		"password": ssh.Password,
 	}
 
 	jsonStr, err := json.Marshal(shellConfig)
@@ -111,23 +112,24 @@ func (c *Collection) AddShell(creds Credentials) {
 // DisplayShellAndOptions is the main user interface where the user can select various inputs to perform actions. The
 // options include connecting to a previously stored session, connecting to a new SSH session, and exiting.
 func (c Collection) DisplayShellAndOptions() {
-	c.refreshCollection()
-	shells := c.ShellCollection.Shells
-	fmt.Println("\nCollected shells:")
-	i := 0
-	options := map[string]string{}
-	for hostname, shell := range shells {
-		fmt.Printf("(%d) %s @ %s\n", i, shell["username"], hostname)
-		options[strconv.Itoa(i)] = hostname
-		i++
-	}
-
-	fmt.Println("\nDo one of the following:")
-	fmt.Println("* Enter an option to connect to that shell via SSH (i.e. 0)")
-	fmt.Println("* Enter a new shell (format: username@hostname.com)")
-	fmt.Println("* Type `exit` to leave the beach")
-	fmt.Println()
 	for { // Keep reading input until an acceptable input is received
+		c.refreshCollection()
+		shells := c.ShellCollection.Shells
+		fmt.Println("\nCollected shells:")
+		i := 0
+		options := map[string]string{}
+		for hostname, sh := range shells {
+			fmt.Printf("(%d) %s @ %s\n", i, sh["username"], hostname)
+			options[strconv.Itoa(i)] = hostname
+			i++
+		}
+
+		fmt.Println("\nDo one of the following:")
+		fmt.Println("* Enter an option to connect to that sh via SSH (i.e. 0)")
+		fmt.Println("* Enter a new sh (format: username@hostname.com)")
+		fmt.Println("* Type `exit` to leave the beach")
+		fmt.Println()
+
 		var in string
 		_, _ = fmt.Scanf("%s", &in)
 		matched, _ := regexp.MatchString(".*(@).*", in)
@@ -140,13 +142,21 @@ func (c Collection) DisplayShellAndOptions() {
 			fmt.Println("Enter a password:")
 			_, _ = fmt.Scanf("%s", &newPassword)
 			encodedPass := base64.StdEncoding.EncodeToString([]byte(newPassword)) // Passwords should always be encoded
-			newCreds := Credentials{Hostname: newHostname, Username: newUsername, Password: encodedPass}
-			c.AddShell(newCreds)
-			// TODO start SSH session
+			newSSH := shell.SSH{Hostname: newHostname, Username: newUsername, Password: encodedPass}
+			//newCreds := Credentials{Hostname: newHostname, Username: newUsername, Password: encodedPass}
+			c.AddShell(newSSH)
+			err := newSSH.StartSession()
+			if err != nil {
+				log.Err.Printf("Error starting session: %v\n", err)
+			}
 
 		} else if hostname, found := options[in]; found { // Connect to a previously stored SSH session
 			fmt.Printf("Logging onto %s\n", hostname)
-			// TODO start SSH session
+			newSSH := shell.SSH{Hostname: hostname, Username: shells[hostname]["username"], Password: shells[hostname]["password"]}
+			err := newSSH.StartSession()
+			if err != nil {
+				log.Err.Printf("Error starting session: %v\n", err)
+			}
 
 		} else if strings.ToLower(in) == "exit" { // Exit the routine
 			fmt.Println("Exiting...")
--- shell/shell.go
+++ shell/shell.go
@@ -1,122 +1,138 @@
 package shell
 
-//var (
-//	hostname string
-//	username string
-//	port     string
-//)
-//
-//func connectToHost(user, host string) (*ssh.Client, *ssh.Session, error) {
-//	var pass string
-//	fmt.Print("Password: ")
-//	fmt.Scanf("%s\n", &pass)
-//
-//	sshConfig := &ssh.ClientConfig{
-//		User: user,
-//		Auth: []ssh.AuthMethod{ssh.Password(pass)},
-//	}
-//
-//	sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-//	client, err := ssh.Dial("tcp", host, sshConfig)
-//	if err != nil {
-//		return nil, nil, err
-//	}
-//
-//	session, err := client.NewSession()
-//	if err != nil {
-//		client.Close()
-//		return nil, nil, err
-//	}
-//
-//	return client, session, nil
-//}
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io"
+	"log"
+	"os"
+)
 
-//hostname = ""
-//username = ""
-//port = "22"
-//
-//log.Printf("Attempting to SSH into %s...\n", hostname)
-//c, s, err := connectToHost(username, hostname+":"+port)
-//defer c.Close()
-//if err != nil {
-//	log.Fatalf("error connecting to host: %v\n", err)
-//}
-//
-//var stdin io.WriteCloser
-//var stdout, stderr io.Reader
-//defer s.Close()
-//
-//stdin, err = s.StdinPipe()
-//if err != nil {
-//	log.Fatalf("error connecting stdin to pipe: %v\n", err)
-//}
-//
-//stdout, err = s.StdoutPipe()
-//if err != nil {
-//	log.Fatalf("error connecting stdout to pipe: %v\n", err)
-//}
-//
-//stderr, err = s.StderrPipe()
-//if err != nil {
-//	log.Fatalf("error connecting stdout to pipe: %v\n", err)
-//}
-//
-//// go routine to pass stdin to shell stdin
-//wr := make(chan []byte, 10)
-//go func() {
-//	for {
-//		select {
-//		case d := <-wr:
-//			_, err := stdin.Write(d)
-//			if err != nil {
-//				fmt.Printf("error writing to stdin: %v\n", err)
-//				break
-//			}
-//		}
-//	}
-//
-//}()
-//
-//// go routine to scan shell stdout
-//go func() {
-//	scanner := bufio.NewScanner(stdout)
-//	for {
-//		if tkn := scanner.Scan(); tkn {
-//			rcv := scanner.Bytes()
-//			raw := make([]byte, len(rcv))
-//			copy(raw, rcv)
-//			fmt.Println(string(raw))
-//		} else if scanner.Err() != nil {
-//			fmt.Printf("error scanning: %v\n", scanner.Err())
-//		} else {
-//			fmt.Printf("\nio.EOF")
-//			break
-//		}
-//	}
-//}()
-//
-//// go routine to scan stderr
-//go func() {
-//	scanner := bufio.NewScanner(stderr)
-//	for scanner.Scan() {
-//		fmt.Println(scanner.Text())
-//	}
-//}()
-//
-//// Open SSH session
-//s.Shell()
-//
-//for {
-//
-//	fmt.Printf("%s@%s $ ", username, hostname)
-//	scanner := bufio.NewScanner(os.Stdin)
-//	scanner.Scan()
-//	text := scanner.Text()
-//	if text == "exit" {
-//		return
-//	}
-//	wr <- []byte(text + "\n")
-//
-//}
-//
-//return
+type SSH struct {
+	Username string
+	Password string
+	Hostname string
+	client   *ssh.Client
+	session  *ssh.Session
+	stdin    io.WriteCloser
+	stdout   io.Reader
+	stderr   io.Reader
+}
+
+func (s *SSH) CreateSession() error {
+	decodedPass, _ := base64.StdEncoding.DecodeString(s.Password)
+	trimmedPass := bytes.TrimSpace(decodedPass)
+
+	sshConfig := &ssh.ClientConfig{
+		User: s.Username,
+		Auth: []ssh.AuthMethod{ssh.Password(string(trimmedPass))},
+	}
+
+	sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+	client, err := ssh.Dial("tcp", s.Hostname+":22", sshConfig)
+	if err != nil {
+		return err
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		_ = client.Close()
+		return err
+	}
+
+	s.client = client
+	s.session = session
+	return nil
+}
+
+func (s *SSH) StartSession() error {
+	//hostname = ""
+	//username = ""
+	//port = "22"
+
+	log.Printf("Attempting to SSH into %s...\n", s.Hostname)
+	err := s.CreateSession()
+	if err != nil {
+		return fmt.Errorf("error connecting to host: %v\n", err)
+	}
+	defer s.client.Close()
+	defer s.session.Close()
+
+	s.stdin, err = s.session.StdinPipe()
+	if err != nil {
+		log.Fatalf("error connecting stdin to pipe: %v\n", err)
+	}
+
+	s.stdout, err = s.session.StdoutPipe()
+	if err != nil {
+		log.Fatalf("error connecting stdout to pipe: %v\n", err)
+	}
+
+	s.stderr, err = s.session.StderrPipe()
+	if err != nil {
+		log.Fatalf("error connecting stdout to pipe: %v\n", err)
+	}
+
+	// go routine to pass stdin to shell stdin
+	wr := make(chan []byte, 10)
+	go func() {
+		for {
+			select {
+			case d := <-wr:
+				_, err := s.stdin.Write(d)
+				if err != nil {
+					fmt.Printf("error writing to stdin: %v\n", err)
+					break
+				}
+			}
+		}
+
+	}()
+
+	// go routine to scan shell stdout
+	go func() {
+		scanner := bufio.NewScanner(s.stdout)
+		for {
+			if tkn := scanner.Scan(); tkn {
+				rcv := scanner.Bytes()
+				raw := make([]byte, len(rcv))
+				copy(raw, rcv)
+				fmt.Println(string(raw))
+			} else if scanner.Err() != nil {
+				fmt.Printf("error scanning: %v\n", scanner.Err())
+			} else {
+				fmt.Printf("\nio.EOF")
+				break
+			}
+		}
+	}()
+
+	// go routine to scan stderr
+	go func() {
+		scanner := bufio.NewScanner(s.stderr)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	// Open SSH session
+	_ = s.session.Shell()
+
+	for {
+
+		fmt.Printf("%s@%s $ ", s.Username, s.Hostname)
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+		if text == "exit" {
+			return nil
+		}
+		wr <- []byte(text + "\n")
+
+	}
+
+	return nil
+}
